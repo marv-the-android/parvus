3bb83d2113a8a510867139a4d6d631e28b86a3c2
marv-the-android <dyingofthelight@hotmail.com>
2020-12-14 14:12:54 +0000

    Begun work on base station code

--- Code/HOUSTON/Real/Houston.py
+++ Code/HOUSTON/Real/Houston.py
@@ -1,2 +1,33 @@
 import urwid
 from time import sleep
+
+class CommandCreate:
+    def Main(self):
+        self.initialise()
+        loop = urwid.MainLoop(urwid.Columns(self.mainview), unhandled_input=self.exit_on_q)
+        loop.run()
+
+    def initialise(self):
+        self.mainview = []
+        self.input()
+        self.output()
+
+    def input(self):
+        txt = urwid.Text("Hello", align="center")
+        fill = urwid.Filler(txt)
+        box = urwid.LineBox(fill)
+        self.mainview.append(box)
+
+    def output(self):
+        txt = urwid.Text("World", align="center")
+        fill = urwid.Filler(txt)
+        box = urwid.LineBox(fill)
+        self.mainview.append(box)
+
+    def exit_on_q(self, key):
+        if key in ["q", "Q"]:
+            raise urwid.ExitMainLoop()
+
+
+new = CommandCreate()
+new.Main()
