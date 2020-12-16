df493d427aa03a8606e4896d8931da341ecfce21
marv-the-android <dyingofthelight@hotmail.com>
2020-12-16 10:32:33 +0000

    Fixed list box updates

--- Code/HOUSTON/Real/Houston.py
+++ Code/HOUSTON/Real/Houston.py
@@ -1,7 +1,11 @@
 import urwid
+import string
 from time import sleep
 
 class CommandCreate:
+    screen = urwid.raw_display.Screen()
+    _,rows = screen.get_cols_rows()
+
     def Main(self):
         self.initialise()
         self.loop = urwid.MainLoop(urwid.Columns(self.mainview), unhandled_input=self.exit_on_q)
@@ -19,21 +23,20 @@ class CommandCreate:
         self.mainview.append(box)
 
     def output(self):
-        self.list1 = []
-        content = urwid.SimpleFocusListWalker(self.list1)
-        listbox = urwid.ListBox(content)
-        adapt = urwid.BoxAdapter(listbox, len(self.list1))
+        self.content = urwid.SimpleFocusListWalker([urwid.Text("Hello world")])
+        listbox = urwid.ListBox(self.content)
+        adapt = urwid.BoxAdapter(listbox, height=self.rows)
         fill = urwid.Filler(adapt)
-        box = urwid.LineBox(fill)
-        self.mainview.append(box)
+        line = urwid.LineBox(fill)
+        self.mainview.append(line)
 
     def exit_on_q(self, key):
         if key in ["q", "Q"]:
             raise urwid.ExitMainLoop()
-        elif key in ["rR"]:
-            self.list1.append(urwid.Text(key))
-            self.loop.draw_screen()
-
+        elif key in string.ascii_lowercase:
+            self.content.append(urwid.Text(key, align="center"))
+        elif key in ["R"]:
+            self.content.clear()
 
 new = CommandCreate()
 new.Main()
--- Code/HOUSTON/Tests/yetanothertest.py
+++ Code/HOUSTON/Tests/yetanothertest.py
@@ -1,9 +1,20 @@
 import urwid
-from time import sleep
 
-text = urwid.Text("WOW")
-listbox = urwid.ListBox([text])
-fill = urwid.Filler(listbox)
+screen = urwid.raw_display.Screen()
+_,rows = screen.get_cols_rows()
 
-loop = urwid.MainLoop(fill)
+
+contentlist = [urwid.Text("Hello world!")]
+
+def keypress(key):
+    content.append(urwid.Text(key))
+    loop.draw_screen()
+
+content = urwid.SimpleFocusListWalker(contentlist)
+listbox = urwid.ListBox(content)
+adapt = urwid.BoxAdapter(listbox, rows)
+fill = urwid.Filler(adapt)
+linebox = urwid.LineBox(fill)
+
+loop = urwid.MainLoop(linebox, unhandled_input=keypress)
 loop.run()
