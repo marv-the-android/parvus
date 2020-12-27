b41d3e8b5c836209f6e4f609e21bef6b51ea1d6d
William Smith <MarvTheAndroid@localhost.localdomain>
2020-12-27 02:03:31 +0000

    Total redo of Houston.py. Now using key presses for command generation. Clearer layout for HOUSTON UI and ability to save to text file have been added. Tired.

--- Code/HOUSTON/Real/Houston.py
+++ Code/HOUSTON/Real/Houston.py
@@ -1,44 +1,78 @@
-import urwid
-import string
-from time import sleep
+import urwid, urwid.numedit
 
-class CommandCreate:
-    screen = urwid.raw_display.Screen()
-    _,rows = screen.get_cols_rows()
-
+class MainScreen:
     def Main(self):
-        self.initialise()
-        self.loop = urwid.MainLoop(urwid.Columns(self.mainview), unhandled_input=self.keypress, palette=[("reversed", "standout", "")])
-        self.loop.run()
+        loop = urwid.MainLoop(self.Setup(), unhandled_input=self.keypress, palette=[("reversed", "standout", "")])
+        loop.run()
 
-    def initialise(self):
-        self.mainview = []
-        self.input()
-        self.output()
+    def Setup(self):
+        top = urwid.Columns([("weight", 1, self.Info()), ("weight", 3, self.CommandList())])
+        mainview = urwid.Pile([("weight", 3, top), ("weight", 1, self.Config())])
+        mainview = urwid.LineBox(mainview)
+        return mainview
 
-    def input(self):
-        txt = urwid.Text("Hello World", align="center")
-        fill = urwid.Filler(txt)
-        box = urwid.LineBox(fill)
-        self.mainview.append(box)
+    def Config(self):
+        values = (("Drive", "1"), ("Reverse", "1"), ("Clockwise", "90"), ("Anticlockwise", "90"))
+        self.configvalues = urwid.SimpleFocusListWalker([])
+        for v in values:
+            temp = urwid.numedit.FloatEdit(("{}: ".format(v[0])), v[1])
+            temp = urwid.AttrMap(temp, None, focus_map="reversed")
+            self.configvalues.append(temp)
+        config = urwid.Columns(self.configvalues)
+        config = urwid.Filler(config)
+        config = urwid.LineBox(config)
+        return config
 
-    def output(self):
-        self.content = urwid.SimpleFocusListWalker([urwid.Text("Hello world")])
-        listbox = urwid.ListBox(self.content)
-        adapt = urwid.BoxAdapter(listbox, height=self.rows)
-        fill = urwid.Filler(adapt)
-        line = urwid.LineBox(fill)
-        self.mainview.append(line)
-
+    def CommandList(self):
+        self.commands = urwid.SimpleFocusListWalker([])
+        commandbox = urwid.ListBox(self.commands)
+        commandbox = urwid.LineBox(commandbox)
+        return commandbox
+    
+    def Info(self):
+        intro = "Welcome to the PARVUS Instruction set development environment!"
+        commandlist = [
+            intro,
+            "Drive : 1",
+            "Reverse : 2",
+            "Clockwise : 3",
+            "Anticlockwise : 4",
+            "Capture : 5",
+            "Exit : F12"
+        ]
+        info = []
+        for c in commandlist:
+            info.append(urwid.Text(c))
+            info.append(urwid.Divider(u" "))
+        info = urwid.Pile(info)
+        info = urwid.Filler(info, valign="top")
+        info = urwid.LineBox(info)
+        return info
+    
     def keypress(self, key):
-        if key in ["Q"]:
+        if key in ["f12"]:
             raise urwid.ExitMainLoop()
-        elif key in string.ascii_lowercase:
-            test = urwid.Button(key)
-            pad = urwid.Padding(test, align="center", width=("relative", 25))
-            self.content.append(urwid.AttrMap(pad, None, focus_map="reversed"))
-        elif key in ["R"]:
-            self.content.clear()
+        elif key in "1 2 3 4".split():
+            numkey = int(key)-1
+            commandparam = ["Drive", "Reverse", "Clockwise", "Anticlockwise"]
+            newcommand = "{0} {1}".format(commandparam[numkey], self.configvalues[numkey].original_widget.value())
+            newcommand = urwid.Button(newcommand)
+            newcommand = urwid.Padding(newcommand, align="left", width=("relative", 50))
+            newcommand = urwid.AttrMap(newcommand, None, focus_map="reversed")
+            self.commands.append(newcommand)
+        elif key in ["5"]:
+            newcommand = urwid.Button("Capture")
+            newcommand = urwid.Padding(newcommand, align="left", width=("relative", 50))
+            newcommand = urwid.AttrMap(newcommand, None, focus_map="reversed")
+            self.commands.append(newcommand)
+        elif key == "delete" and self.commands.get_focus()[1] is not None:
+            temp = self.commands.get_focus()[1]
+            del self.commands[temp]
+        elif key == "f1":
+            with open("test.txt", "w") as f:
+                for c in self.commands:
+                    temp = c.original_widget.original_widget.get_label()
+                    f.write("{}\n".format(temp))
 
-new = CommandCreate()
-new.Main()
+main = MainScreen()
+main.Main()
